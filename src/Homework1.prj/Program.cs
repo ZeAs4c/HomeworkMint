119af31ab099f401d7c6c51255f337e0dd7ae430
alexk <alexk@DESKTOP-Q509TJ6>
2020-04-11 21:13:24 +0300

    Completed tasks

---
+++ src/Homework1.prj/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //if((age > 99) || (age < 0))
+            //{
+            //    Console.WriteLine("Це ошибка вводи число в диапазоне от 0 до 99");
+            //}
+            //else
+            //{
+            //    while(true)
+            //    {
+            //        if((age % 100 == 11) || (age % 100 == 12) || (age % 100 == 13) || (age % 100 == 14) || (age % 10 == 5) || (age % 10 == 6) || (age % 10 == 7) || (age % 10 == 8) || (age % 10 == 9) || (age % 10 == 0))
+            //        {
+            //            Console.WriteLine("Мне " + age + " лет");
+            //            break;
+            //        }
+            //        else if((age % 10 == 2) || (age % 10 == 3) || (age % 10 == 4))
+            //        {
+            //            Console.WriteLine("Мне " + age + " годa");
+            //            break;
+            //        }
+            //        if(age % 10 == 1)
+            //        {
+            //            Console.WriteLine("Мне " + age + " год");
+            //            break;
+            //        }
+            //    }
+            //}
+
+
+
+            //Задача 2
+
+            //int len_side = Convert.ToInt32(Console.ReadLine());
+
+            //if(len_side % 2 == 0)
+            //{
+            //    for(int i = 0; i < len_side; i++)
+            //    {
+            //        for(int j = 0; j <= i; j++)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        Console.WriteLine("");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ЧИСЛО ДОЛЖНО БЫТЬ ЧЕТНЫМ!!!!!!!");
+            //}
+
+
+
+            //Задача 3
+
+            //int len_mas = Convert.ToInt32(Console.ReadLine());
+            //int[] numb = new int[len_mas];
+            //int count_even = 0;
+            //int count_odd = 0;
+
+            //for(int i = 0; i < numb.Length; i++)
+            //{
+            //    Random rnd = new Random();
+            //    numb[i] = rnd.Next(0, 100);
+            //    if(i == 0)
+            //    {
+            //        Console.Write("полученный массив: " + numb[i] + " ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write(numb[i] + " ");
+            //    }
+            //}
+            //Console.WriteLine();
+
+            //for(int i = 0; i < numb.Length; i++)
+            //{
+            //    if(numb[i] % 2 == 0)
+            //    {
+            //        count_even += 1;
+            //    }
+            //    if(numb[i] % 2 != 0)
+            //    {
+            //        count_odd += 1;
+            //    }
+            //}
+
+            //Console.WriteLine("Количество четных чисел = " + count_even);
+            //Console.WriteLine("Количество нечетных чисел = " + count_odd);
+            Console.WriteLine();
+        }
+    }
+}
+
